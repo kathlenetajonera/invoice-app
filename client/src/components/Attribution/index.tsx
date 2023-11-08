491c12689db57ccde1288a89039406c85fbee4db
Kathlene Tajonera <kathlene.tajonera@gmail.com>
2023-11-08 15:40:08 +0800

    chore: added attribution

---
+++ client/src/components/Attribution/index.tsx
@@ -0,0 +1,25 @@
+const Attribution = () => {
+    return (
+        <p className="py-4 text-black text-xs text-center bg-light-bg dark:bg-dark dark:text-white">
+            Challenge by{' '}
+            <a
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+                className="underline"
+            >
+                Frontend Mentor
+            </a>
+            . Coded by{' '}
+            <a
+                href="https://github.com/kathlenetajonera"
+                className="underline"
+                target="_blank"
+            >
+                Kathlene&nbsp;Tajonera
+            </a>
+            .
+        </p>
+    );
+};
+
+export default Attribution;
--- client/src/components/Layout/Container/index.tsx
+++ client/src/components/Layout/Container/index.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import Attribution from '../../Attribution';
 
 type Props = {
     children: ReactNode;
@@ -6,9 +7,12 @@ type Props = {
 
 const Container = ({ children }: Props) => {
     return (
-        <div className="ml-navbar min-h-screen bg-light-bg dark:bg-dark py-[4.5rem] lg:ml-0 lg:mt-navbar lg:py-10 lg:px-4">
-            <div className="max-w-container mx-auto">{children}</div>
-        </div>
+        <>
+            <div className="ml-navbar min-h-screen bg-light-bg dark:bg-dark py-[4.5rem] lg:ml-0 lg:mt-navbar lg:py-10 lg:px-4">
+                <div className="max-w-container mx-auto">{children}</div>
+            </div>
+            <Attribution />
+        </>
     );
 };
 
